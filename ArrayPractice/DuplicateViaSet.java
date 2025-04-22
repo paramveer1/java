e1c7aed454acab4fa71e5df0e1f1a8c7b506fb90
paramveer1 <param1999mangra@gmail.com>
2025-04-22 10:50:48 +0530

    ok2

--- ArrayPractice/DuplicateViaSet.java
+++ ArrayPractice/DuplicateViaSet.java
@@ -1,8 +1,24 @@
 package ArrayPractice;
+import java.util.LinkedHashSet;
 
-public class DuplicateViaSet {
-    public static void main(String[] args) {
-        System.out.println("   ");
+
+public class DuplicateViaSet 
+{
+    public static void main(String[] args) 
+    {
+        int [] a = {3,4,32,335,234,5654,3554,2,35,3,67,465,4,6,877,65,2,2,2,5,5,5,5,5,5,67,67};
+        LinkedHashSet lhs = new LinkedHashSet();
+
+        for(int i : a)
+    
+        {
+            if(!lhs.add(i))
+            {
+                System.out.print(i + " ");
+            }
+
+                
+        }
     }
     
 }
