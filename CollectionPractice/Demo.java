7e97ea83f65c682dc053536d233689a1cfa3c908
paramveer1 <param1999mangra@gmail.com>
2025-04-30 00:01:07 +0530

    new

---
+++ CollectionPractice/Demo.java
@@ -0,0 +1,13 @@
+package CollectionPractice;
+
+import java.util.Vector;
+
+public class Demo {
+    public static void main(String[] args) {
+        Vector v =  new Vector<>();
+        v.add("abhi");
+        v.addFirst("afrin");
+        System.out.print(v);
+    }
+    
+}
