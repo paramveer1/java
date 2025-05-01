aae7f7e054be2cae175764bb39f0257e2440b257
paramveer1 <param1999mangra@gmail.com>
2025-05-01 22:52:11 +0530

    new

--- CollectionPractice/Demo.java
+++ CollectionPractice/Demo.java
@@ -1,12 +1,21 @@
 package CollectionPractice;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class Demo {
     public static void main(String[] args) {
         Vector v =  new Vector<>();
+        ArrayList al = new ArrayList();
+        al.addFirst("pa");
+        System.out.println(al);
+        
         v.add("abhi");
         v.addFirst("afrin");
+                v.addFirst("afrinaaaa");
+
+        v.add("param");
+         v.add("parveeram");
         System.out.print(v);
     }
     
