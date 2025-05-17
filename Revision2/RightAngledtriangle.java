305daef56725bd117e91a2790be6e7adf79bf299
paramveer1 <param1999mangra@gmail.com>
2025-05-17 22:26:58 +0530

    update

---
+++ Revision2/RightAngledtriangle.java
@@ -0,0 +1,21 @@
+package Revision2;
+
+public class RightAngledtriangle
+ {
+    public static void main(String[] args)
+     {
+        
+    
+
+    for(int i = 1;i<= 5;i++)
+    {
+        for(int j = 1;j<= i;j++)
+        {
+            System.out.print(" * ");
+        }
+        System.out.println();
+
+    }
+}
+    
+}
