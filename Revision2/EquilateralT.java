47af217d08ded182be10a831c1e7fa3c5dfa007a
paramveer1 <param1999mangra@gmail.com>
2025-05-18 08:37:11 +0530

    update

---
+++ Revision2/EquilateralT.java
@@ -0,0 +1,38 @@
+public class EquilateralT {
+    public static void main(String[] args) {
+        int a = 5;
+        for(int i = 1;i<=5;i++)
+        {
+            for(int j = 1;j<=9;j++)
+            {
+                if(i+j==6)
+                {
+                    System.out.print("*");
+                }
+                // else
+                // {
+                //     System.out.print(" ");
+                // }
+                else if(j-i==4)
+                {
+                    System.out.print("*");
+                }
+                else if(i==5)
+                {
+                    if(j%2!=0)
+                    System.out.print("*");
+                    else
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+
+               
+            }
+            System.out.println();
+        }
+    }
+    
+}
