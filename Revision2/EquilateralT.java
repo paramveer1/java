11072e7edd6de7b640f0dfee3f4ef13fa53aaa01
paramveer1 <param1999mangra@gmail.com>
2025-05-21 22:53:38 +0530

    update

--- Revision2/EquilateralT.java
+++ Revision2/EquilateralT.java
@@ -1,6 +1,6 @@
 public class EquilateralT {
     public static void main(String[] args) {
-        int a = 5;
+        
         for(int i = 1;i<=5;i++)
         {
             for(int j = 1;j<=9;j++)
