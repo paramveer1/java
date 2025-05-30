76f3043d898f4659fa79b22bf1f479ab065631d6
paramveer1 <param1999mangra@gmail.com>
2025-05-30 17:44:59 +0530

    update

--- Demo.java
+++ Demo.java
@@ -1,10 +1,31 @@
 public class Demo {
 
+    String name = "pa";
+    int id;
+    static int p = 0;
+
+    void ok(){
+        System.out.println(name);
+    }
+
+  void set(String name,int id)
+  {
+    this.name = name;
+    this.id =  id;
+  }
 
     
     public static void main(String[] args) {
+        // p = 9;
+        // System.out.println(p);
 
-        System.out.println("paramveer");
+
+
+       Demo d = new Demo();
+    //    d.set("param",555);
+
+    //    System.out.println(name);
+    d.ok();
         
         
     }
